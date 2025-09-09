3ad3c9a13f319d817a4c0174f86aa61459a729b3
HowardHoang <howard.hoang@vatek.asia>
2025-09-09 16:17:29 +0700

    loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,11 +1,11 @@
 export default function Loading() {
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-background">
-            <div className="flex flex-col items-center gap-4">
-                <div className="w-12 h-12 border-4 border-icon border-t-transparent rounded-full animate-spin"></div>
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-background">
+      <div className="flex flex-col items-center gap-4">
+        <div className="w-12 h-12 border-4 border-icon border-t-transparent rounded-full animate-spin"></div>
 
-                <p className="text-white text-sm">Loading....</p>
-            </div>
-        </div>
-    );
+        <p className="text-text-body text-sm">Loading....</p>
+      </div>
+    </div>
+  );
 }
